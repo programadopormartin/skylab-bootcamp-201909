5440ed01b33e13a36ec7c9b1e3b2fda92c756e89
Martin Garcia Blanco <programadopormartin@gmail.com>
2019-10-23 17:46:46 +0200

    boost duck-app-react

--- staff/martin-garcia/duck-app-react/components/search/index.jsx
+++ staff/martin-garcia/duck-app-react/components/search/index.jsx
@@ -1,13 +1,25 @@
-function Search(){
+function Search({onSearch,error, results, on}){
 
     return  <section className="search" >
                 <nav className="search__nav nav">
-                    <form className="myForm">
+                    <form className="myForm" onSubmit = {event=>{
+                        event.preventDefault()
+                        debugger
+                        const query = event.target.query.value
+                        debugger
+                        onSearch(query)
+
+                    }}>
                         <input className="myForm__search" type="search" name="query" placeholder="Are you looking for a special Duck?"/> 
                         <button> <i className="fas fa-search myForm__button" id="searchButton" aria-hidden="true"></i></button>
                     </form>
                 </nav>
-                <main className="search__main main"> </main>
+                <main className="search__main main">
+                    {results && onResultsRender(results)}
+                </main>
+
+                {error && <Feedback message={error} />} 
+
             </section>
 
 }
