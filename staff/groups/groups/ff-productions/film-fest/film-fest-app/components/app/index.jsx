2cefc09a495dfb7e0cdf3a1cf65d56ba7ce2cbc4
Martin Garcia Blanco <programadopormartin@gmail.com>
2019-10-28 17:10:50 +0100

    dummy register done

--- staff/groups/groups/ff-productions/film-fest/film-fest-app/components/app/index.jsx
+++ staff/groups/groups/ff-productions/film-fest/film-fest-app/components/app/index.jsx
@@ -4,18 +4,36 @@ class App extends Component {
 
         constructor(){
             super()
-            this.state = { view: 'landing', error: undefined, query: undefined }
+            this.state = { view: 'register', error: undefined, query: undefined }
+        }
+
+        handleRegister = (name,surname,email,password,passwordConfirmation)=>{
+            debugger
+             try{
+                console.log('handleRegister')
+                registerUser(name,surname,email,password,passwordConfirmation, error=>{
+                    if(error) console.log(error.message)
+                    else console.log('user inserted')
+                })
+                console.log('handleRegister')
+            } catch(error){
+                debugger
+                console.log(error.message)
+            } 
+        }
+
+        handleGoLogin = ()=>{
+            console.log('handleGoLogin')
         }
 
 
 
-
         render() {
-            const { } = this
+            const { state: {view}, handleRegister, handleGoLogin } = this
 
             return <>     
                 <Header/>
-
+                {view === 'register' && <Register onRegister={handleRegister} onGoLogin={handleGoLogin}/>} 
                 <Footer/>
                </>
         }
---
+++ staff/groups/groups/ff-productions/film-fest/film-fest-app/logic/register-user/index.js
@@ -0,0 +1,20 @@
+function registerUser(name, surname, email, password, password, passwordConfirmation, callback) {
+    if (typeof name !== 'string') throw new TypeError(name + ' is not a string')
+    if (!name.trim().length) throw new ContentError('name is empty or blank')
+    if (typeof surname !== 'string') throw new TypeError(surname + ' is not a string')
+    if (!surname.trim().length) throw new ContentError('surname is empty or blank')
+    if (typeof email !== 'string') throw new TypeError(email + ' is not a string')
+    if (!email.trim().length) throw new ContentError('e-mail is empty or blank')
+    if (typeof password !== 'string') throw new TypeError(password + ' is not a string')
+    if (!password.trim().length) throw new ContentError('password is empty or blank')
+    if (typeof passwordConfirmation !== 'string') throw new TypeError(passwordConfirmation + ' is not a string')
+    if (!passwordConfirmation.trim().length) throw new ContentError('passwordConfirmation is empty or blank')
+    if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function')
+
+
+
+    call('POST', undefined, 'https://skylabcoders.herokuapp.com/api/user', { name, surname, username: email, password }, result => {
+        debugger
+        result.error ? callback(new Error(result.error)) : callback();
+    })
+}
