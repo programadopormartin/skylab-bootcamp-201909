290b37fc09c07fe9b9224a21e7a706a4448ad4e9
Martin Garcia Blanco <programadopormartin@gmail.com>
2019-11-14 15:31:40 +0100

    implemented create-task logic

---
+++ staff/martin-garcia/tasks/tasks-api/logic/create-task/index.js
@@ -0,0 +1,36 @@
+const users = require('../../data/users')()
+const tasks = require('../../data/tasks')()
+const validate = require('../../utils/validate')
+const { NotFoundError } = require('../../utils/errors')
+const uuid = require('uuid/v4')
+
+
+module.exports = function(userId, title, description) {
+    validate.string(userId)
+    validate.string.notVoid('userId', userId)
+    validate.string(title)
+    validate.string.notVoid('title', title)
+    validate.string(description)
+    validate.string.notVoid('description', description)
+
+    return new Promise((resolve, reject) => {
+        const user = users.data.find(user => user.id === userId)
+
+        if (!user) return reject(new NotFoundError(`user with id ${userId} not found`))
+
+        const task = {
+            id: uuid(),
+            user: userId,
+            title,
+            description,
+            status: 'TODO',
+            date: new Date
+        }
+
+        tasks.data.push(task)
+
+        tasks.persist()
+            .then(() => resolve(task.id))
+            .catch(reject)
+    })
+}
