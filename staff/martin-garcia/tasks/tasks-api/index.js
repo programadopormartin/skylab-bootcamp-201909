b4517f5916b5e0e1565d81343a42a0e0c4ac1402
Martin Garcia Blanco <programadopormartin@gmail.com>
2019-11-14 13:50:08 +0100

    project implementes jsonWebToken security

--- staff/martin-garcia/tasks/tasks-api/index.js
+++ staff/martin-garcia/tasks/tasks-api/index.js
@@ -1,13 +1,16 @@
+require('dotenv').config()
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const { name, version } = require('./package.json')
 const users = require('./data/users')()
 const { registerUser, authenticateUser, retrieveUser } = require('./logic')
 const { ConflictError, CredentialsError, NotFoundError } = require('./utils/errors')
-
+const jwt = require('jsonwebtoken')
+const { JsonWebTokenError } = jwt
 const api = express()
 const jsonBodyParser = bodyParser.json()
-const { argv: [, , port = 9090] } = process
+const { argv: [, , port], env: { SECRET, PORT = port || 9090 } } = process
 
 api.post('/users', jsonBodyParser, (req, res) => {
     debugger
@@ -26,32 +29,20 @@ api.post('/users', jsonBodyParser, (req, res) => {
     }
 })
 
-api.post('/auth'), jsonBodyParser, (req, res) => {
+api.post('/auth', jsonBodyParser, (req, res) => {
     const { body: { username, password } } = req
     debugger
     try {
         debugger
         authenticateUser(username, password)
-            .then(id => res.json({ message: 'user authenticated succesfully', id }))
+            .then(id => {
+                const token = jwt.sign({ sub: id }, SECRET, { expiresIn: '1d' })
+
+                res.json({ token })
+            })
             .catch(error => {
                 if (error instanceof CredentialsError)
-                    return res.status(404).json({ message: error.message })
-                res.status(500).json({ message: error.message })
-            })
-
-    } catch ({ message }) {
-        res.status(400).json({ message })
-    }
-}
-
-api.get('/users/:id', (req, res) => {
-    const { params: { id } } = req
-    try {
-        retrieveUser(id)
-            .then(user => res.json({ message: 'user retrieved successfully', id }))
-            .catch(error => {
-                if (error instanceof NotFoundError)
-                    return res.status(404).json({ message: error.message })
+                    return res.status(401).json({ message: error.message })
                 res.status(500).json({ message: error.message })
             })
 
@@ -60,5 +51,31 @@ api.get('/users/:id', (req, res) => {
     }
 })
 
+api.get('/users', (req, res) => {
+    const { headers: { authorization } } = req
+    try {
+        if (!authorization) return new CredentialsError('no token provided')
+
+        const [, token] = authorization.split(' ')
+        const { sub: id } = jwt.verify(token, SECRET)
+
+        retrieveUser(id)
+            .then(user => res.json({ user }))
+            .catch(error => {
+                if (error instanceof NotFoundError)
+                    return res.status(404).json({ message: error.message })
+                res.status(500).json({ message: error.message })
+            })
+
+    } catch (error) {
+        const { message } = error
+
+        if (error instanceof JsonWebTokenError || error instanceof CredentialsError)
+            return res.status(401).json({ message })
+
+        res.status(400).json({ message })
+    }
+})
+
 users.load()
-    .then(() => api.listen(port, () => console.log(`${name} ${version} up and running on port ${port}`)))
+    .then(() => api.listen(PORT, () => console.log(`${name} ${version} up and running on port ${PORT}`)))
