cc522a81054e067a118441012d75e7a73cc53ae8
Martin Garcia Blanco <programadopormartin@gmail.com>
2019-11-26 10:04:44 +0100

    working on mongoose schemas

---
+++ staff/martin-garcia/theatera/theatera-data/schemas/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    account: require('./user'),
+    chat: require('./chat'),
+    comment: require('./comment'),
+    company: require('./company'),
+    experienceItem: require('./experienceItem'),
+    message: require('./message'),
+    post: require('./post'),
+    user: require('./user'),
+}
---
+++ staff/martin-garcia/theatera/theatera-data/schemas/user.js
@@ -0,0 +1,53 @@
+const { Schema, ObjectId } = require('mongoose')
+
+module.exports = new Schema({
+    name: {
+
+    },
+    image: {
+
+    },
+    email: {
+
+    },
+    password: {
+
+    },
+    lastaccess: {
+
+    },
+    phone: {
+
+    },
+    address: {
+
+    },
+    city: {
+
+    },
+    website: {
+
+    },
+    introduction: {
+
+    },
+    description: {
+
+    },
+    skills: {},
+    experiences: {
+
+    },
+    rol: {
+
+    },
+    specificInfo: {
+
+    },
+    chats: {
+
+    },
+    posts: {
+
+    }
+})
