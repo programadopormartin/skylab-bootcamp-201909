374c917ebbd5226dedf02c4cd9b321396cda1944
Martin Garcia Blanco <programadopormartin@gmail.com>
2019-11-28 18:24:37 +0100

    implement retrievePostById test and logic

--- staff/martin-garcia/theatera/theatera-api/logic/retrieve-post-by-id/index.spec.js
+++ staff/martin-garcia/theatera/theatera-api/logic/retrieve-post-by-id/index.spec.js
@@ -4,7 +4,7 @@ const { expect } = require('chai')
 const { random } = Math
 const retrievePostById = require('.')
 const { errors: { NotFoundError } } = require('theatera-util')
-const { database, models: { User } } = require('theatera-data')
+const { database, models: { User, Post } } = require('theatera-data')
 
 describe('logic - retrieve-post', () => {
     before(() => database.connect(TEST_DB_URL))
@@ -18,39 +18,54 @@ describe('logic - retrieve-post', () => {
         random() > 0.5 ? rol = 'COMPANY' : rol = 'PERSON'
         introduction = `introduction-${random()}`
 
-        await User.deleteMany()
+        body = `body-${random()}`
+        date = new Date
+        type = 'ARTICLE'
+        const likes = []
+        const comments = []
 
-        const user = await User.create({ name, email, password, rol, introduction })
 
-        id = user.id
+
+        await Promise.all([User.deleteMany()])
+        let user = await User.create({ name, email, password, rol, introduction })
+        userId = user.id
+        _userId = user._id
+        post = await new Post({ body, date, type, user: _userId, })
+        postId = post.id
+
+
+
+        user.posts.push(post)
+
+        await user.save()
+        user = await User.findById(userId)
+        const a = "asdfs"
     })
+
 
     it('should succeed on correct user id', async() => {
-        const user = await retrievePostById(id)
-        let _introduction
-        introduction.length > 20 ? _introduction = introduction.slice(0, 20) + '...' : _introduction = introduction
 
 
-        expect(user).to.exist
-        expect(user.id).to.equal(id)
-        expect(user.id).to.be.a('string')
-        expect(user._id).to.not.exist
-        expect(user.name).to.equal(name)
-        expect(user.name).to.be.a('string')
-        expect(user.password).to.be.undefined
-        expect(user.introduction).to.equal(_introduction)
-        expect(user.introduction).to.be.a('string')
+        const _post = await retrievePostById(userId, postId)
 
 
-        /* img test? */
+        expect(_post).to.exist
+        expect(_post.post.body).to.equal(body)
+        expect(_post.post.body).to.be.a('string')
+
+
+        expect(_post.post.date).to.be.an.instanceOf(Date)
+
+        expect(_post.post.type).to.equal(type)
+        expect(_post.post.type).to.be.a('string')
 
     })
 
-    it('should fail on wrong user id', async() => {
+    it('should fail on wrong user userId', async() => {
         const id = '012345678901234567890123'
 
         try {
-            await retrievePostById(id)
+            await retrievePostById(id, postId)
 
             throw Error('should not reach this point')
         } catch (error) {
@@ -59,6 +74,7 @@ describe('logic - retrieve-post', () => {
             expect(error.message).to.equal(`user with id ${id} not found`)
         }
     })
+
 
     // TODO other cases
 
