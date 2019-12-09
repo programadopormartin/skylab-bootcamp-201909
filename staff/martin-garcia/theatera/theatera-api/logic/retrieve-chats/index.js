bf878a13b1c95590c7b523051fe89b0aa962f945
Martin Garcia Blanco <programadopormartin@gmail.com>
2019-12-09 01:41:28 +0100

    update retrieveChats API logic

--- staff/martin-garcia/theatera/theatera-api/logic/retrieve-chats/index.js
+++ staff/martin-garcia/theatera/theatera-api/logic/retrieve-chats/index.js
@@ -7,19 +7,21 @@ module.exports = function(userId) {
     validate.string.notVoid('userId', userId)
     if (!ObjectId.isValid(userId)) throw new ContentError(`${userId} is not a valid id`)
 
-
     return (async() => {
-
         const user = await User.findById(userId)
         if (!user) throw new NotFoundError(`user with id ${userId} not found`)
 
-        const chats = await Chat.find({ "users": { $in: [userId] } })
+        const chats = await Chat.find({ "users": { $in: [userId] }}).populate({path:'users',
+         model: 'User'})
 
+        chats.forEach(chat=>{
+            const index = chat.users.map((user,index)=>{
+                return user.id===userId && index
+            })
+            chat.users.splice(index,1)
+        })
 
         if (!chats) throw new NotFoundError(`chat with id ${chatId} not found`)
-
         return chats
-
-
     })()
 }
